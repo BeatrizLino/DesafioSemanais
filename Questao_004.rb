6843fe4a751bd6477e4354d2655d276825a5fc2b
Beatrizlino <beatrizcarlos207@gmail.com>
2020-09-13 19:41:01 -0300

    Correçao do codigo

--- Questao_004.rb
+++ Questao_004.rb
@@ -5,15 +5,17 @@ s = d
 
 d1 = s[0]
 d2 = s[1]
+i1 = s[2]
 
 m1 = s[3]
 m2 = s[4]
+i2 = s[5]
 
 y1 = s[6]
 y2 = s[7]
 y3 = s[8]
 y4 = s[9]
 
-soma = d1.to_i+d2.to_i+m1.to_i+m2.to_i+y1.to_i+y2.to_i+y3.to_i+y4.to_i
+soma = d1.to_i+d2.to_i+i1.to_i+m1.to_i+m2.to_i+i2.to_i+y1.to_i+y2.to_i+y3.to_i+y4.to_i
 
 puts "\nO somatorio da sua data de nascimento é: #{soma}"
