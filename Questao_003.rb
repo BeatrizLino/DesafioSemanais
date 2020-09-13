19ca6bd1b59c2d9442ebce5f2c09842774a34251
Beatrizlino <beatrizcarlos207@gmail.com>
2020-09-13 20:33:33 -0300

    Correçao no codigo

--- Questao_003.rb
+++ Questao_003.rb
@@ -3,4 +3,12 @@ nome = gets.chomp
 
 puts "\nSeu nome: #{nome}"
 
-puts "Metade do seu nome: #{first_part = nome [0..3]}"
+t1 = nome.size%2
+
+if t1 == 0
+    t2 = (nome.size-1)/2
+    puts "Metade do seu nome: #{string = nome [0..t2]}"
+else
+    t2 = (nome.size-1)/2
+    puts "Metade do seu nome: #{string = nome [0..t2]}"
+end
